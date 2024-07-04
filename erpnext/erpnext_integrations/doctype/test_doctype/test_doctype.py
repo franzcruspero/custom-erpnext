72b5b8ffc716f0367dea5044aff083b5c2fccd25
Franz Andrei Cruspero <franz.cruspero@gmail.com>
2024-07-05 03:42:23 +0800

    added new doctype

---
+++ erpnext/erpnext_integrations/doctype/test_doctype/test_doctype.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class TestDoctype(NestedSet):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		amended_from: DF.Link | None
+		is_group: DF.Check
+		lft: DF.Int
+		old_parent: DF.Link | None
+		parent_test_doctype: DF.Link | None
+		rgt: DF.Int
+	# end: auto-generated types
+
+	pass
